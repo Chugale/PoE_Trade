90c1aade00c422e1dc1a49266aa4909a113ea1c9
Eric Lee <leric7135@gmail.com>
2023-08-17 10:03:25 -0700

    update to search bar, incorporates enter as reset

--- PoE_Trade_OG/client/src/components/SearchBar.jsx
+++ PoE_Trade_OG/client/src/components/SearchBar.jsx
@@ -5,16 +5,23 @@ const Search = ({searchInput, setSearchInput}) => {
   const handleSearch = (event) => {
     setSearchInput(event.target.value);
 }
+  const resetInput = (event) => {
+    event.preventDefault()
+    setSearchInput("")
+  }
 
   return (
-    <label className="m-8">
-      <input type="text"
-        size="32"
-        placeholder="Search by name..."
+
+    <form onSubmit={resetInput}>
+      <input className="px-2"
+        type="text"
+        size="28"
+        placeholder="Search by gem name..."
         value={searchInput}
         onChange={handleSearch}>
       </input>
-    </label>
+      <button className="border rounded-md bg-black text-white px-2">Press enter to reset</button>
+    </form>
   )
 
 }
